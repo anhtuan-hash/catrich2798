2205d52d6dabc7f27c996c51b2069312ee09b459
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-18 11:26:26 +0700

    Load fixed custom app dialog actions

--- src/pages/WebApps.jsx
+++ src/pages/WebApps.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WebAppsBase from './WebAppsBase.jsx';
 import CustomAppsExtension from './CustomAppsExtension.jsx';
 import '../ui-core/styles/apps-custom-sharing-v12410.css';
+import '../ui-core/styles/apps-dialog-actions-visible.css';
 
 export default function WebApps(props) {
   return (
