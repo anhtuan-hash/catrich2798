4c1f7bfa7ef721ea6761b696d255ef33a4978f05
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-22 17:43:15 +0700

    Load notification toggle styles

--- department-app/src/main.jsx
+++ department-app/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './styles.css';
 import './laptop-scale.css';
 import './macbook-readable.css';
+import './notification-toggle.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
