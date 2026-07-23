fe5e24a5b4ac44ebf31eb2e24241ec172dfc9a7c
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-23 19:59:39 +0700

    Load Google Material Apps directory styling

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -12,6 +12,7 @@ import './GlobalHomeDashboardRemoval.css';
 import './GlobalHomeGooglePolish.css';
 import './GlobalHome16x9Fit.css';
 import './GlobalHomeOriginalFooter.css';
+import './GlobalAppsGoogle.css';
 
 export default function GlobalFlatNavigation(props) {
   return (
