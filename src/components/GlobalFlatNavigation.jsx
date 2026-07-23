91ae8524e8677c8ed2ff6a439d9ae91ca082e560
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-23 21:35:56 +0700

    Mount the in-app news reader navigation guard

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -3,6 +3,7 @@ import Navigation from './GlobalCompactNavigation.jsx';
 import GlobalGamesNavigationTab from './GlobalGamesNavigationTab.jsx';
 import GlobalWorkHubNotificationBridge from './GlobalWorkHubNotificationBridge.jsx';
 import GlobalEnglishHubBrand from './GlobalEnglishHubBrand.jsx';
+import GlobalNewsReaderGuard from './GlobalNewsReaderGuard.jsx';
 import './GlobalGoogleMaterialOverride.css';
 import './GlobalNotificationCenter.css';
 import './GlobalNotificationCenterLayoutFix.css';
@@ -25,6 +26,7 @@ export default function GlobalFlatNavigation(props) {
       <GlobalGamesNavigationTab {...props} />
       <GlobalWorkHubNotificationBridge currentUser={props.currentUser} language={props.language} />
       <GlobalEnglishHubBrand />
+      <GlobalNewsReaderGuard />
     </>
   );
 }
