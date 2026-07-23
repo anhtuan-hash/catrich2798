bc1c27c7593d6b00b81d7989b412179325bbd647
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-23 18:06:59 +0700

    Load notification center Material styles

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from './GlobalCompactNavigation.jsx';
 import GlobalWorkHubNotificationBridge from './GlobalWorkHubNotificationBridge.jsx';
 import './GlobalGoogleMaterialOverride.css';
+import './GlobalNotificationCenter.css';
 
 export default function GlobalFlatNavigation(props) {
   return (
