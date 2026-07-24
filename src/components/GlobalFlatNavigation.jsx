09eab90d0e75b6704c95d1ab37476d33d4f7486d
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-24 14:05:15 +0700

    Keep avatar previews visible in Settings

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import Navigation from './GlobalCompactNavigation.jsx';
 import GlobalUserProfileSettingsBridge from './GlobalUserProfileSettingsBridge.jsx';
+import GlobalUserProfilePreviewGuard from './GlobalUserProfilePreviewGuard.jsx';
 import GlobalAiWebsiteLauncher from './GlobalAiWebsiteLauncher.jsx';
 import GlobalNewsNavigationTab from './GlobalNewsNavigationTab.jsx';
 import GlobalGamesNavigationTab from './GlobalGamesNavigationTab.jsx';
@@ -51,6 +52,7 @@ export default function GlobalFlatNavigation(props) {
     <>
       <Navigation {...props} />
       <GlobalUserProfileSettingsBridge {...props} />
+      <GlobalUserProfilePreviewGuard />
       <GlobalAiWebsiteLauncher {...props} />
       <GlobalNewsNavigationTab {...props} />
       <GlobalGamesNavigationTab {...props} />
