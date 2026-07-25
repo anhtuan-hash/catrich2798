f46de7202afb4fa887ec3d54b87421b5e4d417db
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-25 22:07:35 +0700

    Mount proposal 2 navigation design

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -14,6 +14,7 @@ import GlobalWorkHubNotificationBridge from './GlobalWorkHubNotificationBridge.j
 import GlobalWorkScheduleBridge from './GlobalWorkScheduleBridge.jsx';
 import GlobalEnglishHubBrand from './GlobalEnglishHubBrand.jsx';
 import GlobalHomeBriefingExtras from './GlobalHomeBriefingExtras.jsx';
+import GlobalNavigationProposal2Bridge from './GlobalNavigationProposal2Bridge.jsx';
 import './GlobalGoogleMaterialOverride.css';
 import './GlobalNotificationCenter.css';
 import './GlobalNotificationCenterLayoutFix.css';
@@ -54,6 +55,7 @@ import './GlobalSeparatedPills.css';
 import './GlobalScrollPerformance.css';
 import './GlobalTickerRestore.css';
 import './GlobalWorkScheduleModern.css';
+import './GlobalNavigationProposal2.css';
 
 const GlobalWorkScheduleCompatibleCenter = lazy(() => import('./GlobalWorkScheduleCompatibleCenter.jsx'));
 const GlobalWorkScheduleTemplatePanel = lazy(() => import('./GlobalWorkScheduleTemplatePanel.jsx'));
@@ -65,6 +67,7 @@ export default function GlobalFlatNavigation(props) {
   return (
     <>
       <Navigation {...props} />
+      <GlobalNavigationProposal2Bridge language={props.language} setLanguage={props.setLanguage} />
       <GlobalHomeBriefingExtras route={props.route} language={props.language} />
       <GlobalMotionCoreBridge route={props.route} />
       <GlobalUserProfileSettingsBridge {...props} />
