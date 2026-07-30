eeeb2a458f95942708b31708c80060ce24efccdb
anhtuan-hash <anhtuan@pek.edu.vn>
2026-07-30 14:27:59 +0700

    Load flat relief hero styles after stable layout

--- src/pages/WebAppsAndroidDrawer.jsx
+++ src/pages/WebAppsAndroidDrawer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WebAppsRedesign from './WebAppsRedesign.jsx';
 import '../styles/apps-android-drawer.css';
 import '../styles/apps-hero-clean-v3.css';
+import '../styles/apps-hero-flat-relief-v4.css';
 
 export default function WebAppsAndroidDrawer(props) {
   return <WebAppsRedesign {...props} />;
