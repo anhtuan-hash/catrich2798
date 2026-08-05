4134f8c98fe0fd91e0fa5f3e389c997c46c7030d
anhtuan-hash <anhtuan@pek.edu.vn>
2026-08-05 21:53:27 +0700

    Nhập an toàn danh sách 27 lớp mới
    * Thêm gói dữ liệu mã hóa 27 lớp
    
    * Thêm trình nhập mã hóa 27 lớp
    
    * Kích hoạt trình nhập 27 lớp
    
    * Ổn định nhận diện danh mục đã nhập

--- src/applicationBootstrap.jsx
+++ src/applicationBootstrap.jsx
@@ -1,4 +1,5 @@
 import './fourClassLocalPurge.js';
+import './secureClassImportBootstrap.js';
 import './styles/MotionRestore.css';
 import './components/GlobalHomeroomMaterial3Refinement.css';
 import {
