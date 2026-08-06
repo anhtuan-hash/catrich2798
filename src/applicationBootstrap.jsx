774c7a14ad5cd32d670be5410150a975de753640
anhtuan-hash <anhtuan@pek.edu.vn>
2026-08-06 23:17:14 +0700

    Load Brian brand hover runtime

--- src/applicationBootstrap.jsx
+++ src/applicationBootstrap.jsx
@@ -1,6 +1,7 @@
 import './fourClassLocalPurge.js';
 import './directClassRosterImportBootstrap.js';
 import './styles/MotionRestore.css';
+import './brianBrandMagneticHover.js';
 import './components/GlobalHomeroomMaterial3Refinement.css';
 import {
   installSiteFontFromCache,
