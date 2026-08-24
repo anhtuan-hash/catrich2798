d408c0677c41706593f4e99a5a8e621f06d931a2
anhtuan-hash <anhtuan@pek.edu.vn>
2026-08-24 22:23:47 +0700

    fix: redirect retired games route from global navigation bridge

--- src/components/GlobalGamesNavigationTab.jsx
+++ src/components/GlobalGamesNavigationTab.jsx
@@ -1,3 +1,11 @@
-export default function GlobalGamesNavigationTab() {
+import { useEffect } from 'react';
+
+export default function GlobalGamesNavigationTab({ route = '' }) {
+  useEffect(() => {
+    if (route === 'games' && typeof window !== 'undefined' && window.location.hash !== '#/apps') {
+      window.location.hash = '#/apps';
+    }
+  }, [route]);
+
   return null;
 }
