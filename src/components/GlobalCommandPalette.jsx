547d02eae0dc94d6f2d526ade0079a9586dc127a
anhtuan-hash <anhtuan@pek.edu.vn>
2026-08-25 07:48:03 +0700

    Load modern command palette visual layer

--- src/components/GlobalCommandPalette.jsx
+++ src/components/GlobalCommandPalette.jsx
@@ -1,4 +1,5 @@
 import GlobalCommandPaletteV21 from './GlobalCommandPaletteV21.jsx';
 import './GlobalCommandPaletteV22HoverFix.css';
+import './GlobalCommandPaletteModern2026.css';
 
 export default GlobalCommandPaletteV21;
