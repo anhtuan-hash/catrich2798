01b8c4421d1dd6f175cef31fa7732cb0b6c5469b
anhtuan-hash <anhtuan@pek.edu.vn>
2026-08-25 07:53:24 +0700

    style: load command center color roles

--- src/components/GlobalCommandPalette.jsx
+++ src/components/GlobalCommandPalette.jsx
@@ -1,5 +1,6 @@
 import GlobalCommandPaletteV21 from './GlobalCommandPaletteV21.jsx';
 import './GlobalCommandPaletteV22HoverFix.css';
 import './GlobalCommandPaletteModern2026.css';
+import './GlobalCommandPaletteColorRoles2026.css';
 
 export default GlobalCommandPaletteV21;
