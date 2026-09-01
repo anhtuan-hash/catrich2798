1108832be1a7567f304226e4b7940250d27ffdd3
anhtuan-hash <anhtuan@pek.edu.vn>
2026-09-01 18:57:40 +0700

    fix: bootstrap isolated welcome scene alignment runtime

--- src/tabResumeStability.js
+++ src/tabResumeStability.js
@@ -1,4 +1,5 @@
 import './globalWindowsPhoneLoadingIndicator.js';
+import './welcomeSceneAlignmentFix.js';
 import { reportPwaRefreshNeeded } from './utils/pwa.js';
 
 const INSTALL_KEY = '__besTabResumeStabilityInstalled';
