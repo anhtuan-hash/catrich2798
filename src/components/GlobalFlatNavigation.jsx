26e5a40e7ca4e45e92666721c07975d4dddcf924
anhtuan-hash <anhtuan@pek.edu.vn>
2026-09-01 23:07:45 +0700

    feat: load global Hero Theme Studio layer styles

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -42,6 +42,7 @@ import './GlobalNotificationCenterRemoval.css';
 import './GlobalWindowsPhone8Loading.css';
 import './GlobalNotificationCenterFinalRemoval.css';
 import './GlobalHomeSparkleButton.css';
+import './GlobalHeroThemeStudio.css';
 import './homeroom/HomeroomCompactDensity.css';
 import '../styles/GlobalLayout16x9Authority.css';
 
