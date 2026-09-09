f7b1f5ff94cc51a4dd741bb6478dedecd17f6fe4
anhtuan-hash <anhtuan@pek.edu.vn>
2026-09-09 16:00:21 +0700

    Revert "Attendance: improve mobile readability"
    Rollback production to the exact tree from 127a1547ccc4f4463bfada5dc6931fa491300466 after mobile layout regression.

--- src/tabResumeStability.js
+++ src/tabResumeStability.js
@@ -5,8 +5,6 @@ import './attendanceQuickClassListCollapse.js';
 import './attendancePostConfirmEditBootstrap.js';
 import './attendanceCalendarDirectEntryBootstrap.js';
 import './attendanceLegacyMonthlyCalendarCleanup.js';
-import './components/attendance/AttendanceIphoneReadable.css';
-import './components/attendance/AttendanceMobileV2.css';
 import { reportPwaRefreshNeeded } from './utils/pwa.js';
 
 const INSTALL_KEY = '__besTabResumeStabilityInstalled';
