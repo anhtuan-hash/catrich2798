e635a43e1b5c225175d26f62801c3ef7cef01b8d
anhtuan-hash <anhtuan@pek.edu.vn>
2026-09-13 23:53:54 +0700

    fix: preserve TTCM navigation order with mobile sidecar

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -21,6 +21,7 @@ import GlobalDashboardNavigationTab from './GlobalDashboardNavigationTab.jsx';
 import GlobalHomeroomNavigationTab from './GlobalHomeroomNavigationTab.jsx';
 import GlobalGradebookNavigationTab from './GlobalGradebookNavigationTab.jsx';
 import GlobalReportsNavigationTab from './GlobalReportsNavigationTab.jsx';
+import GlobalTtcmNavigationTab from './GlobalTtcmNavigationTab.jsx';
 import GlobalTtcmMobileAdapter from './GlobalTtcmMobileAdapter.jsx';
 import GlobalAttendanceNavigationTab from './GlobalAttendanceNavigationTab.jsx';
 import GlobalAttendanceAdminPersistenceBridge from './GlobalAttendanceAdminPersistenceBridge.jsx';
@@ -104,7 +105,8 @@ export default function GlobalFlatNavigation(props) {
       <GlobalHomeroomNavigationTab {...props} />
       <GlobalGradebookNavigationTab {...props} />
       <GlobalReportsNavigationTab {...props} />
-      <GlobalTtcmMobileAdapter {...props} />
+      <GlobalTtcmNavigationTab {...props} />
+      <GlobalTtcmMobileAdapter />
       <GlobalAttendanceNavigationTab {...props} />
       <GlobalAttendanceAdminPersistenceBridge {...props} />
       {!mobile ? <GlobalDashboardFooterBridge route={props.route} language={props.language} /> : null}
