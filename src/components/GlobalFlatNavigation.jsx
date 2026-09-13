92f42d25e10ea7ba9f2dcf2cf6a1086b6382428f
anhtuan-hash <anhtuan@pek.edu.vn>
2026-09-13 23:51:17 +0700

    feat: route TTCM through mobile presentation adapter

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -21,7 +21,7 @@ import GlobalDashboardNavigationTab from './GlobalDashboardNavigationTab.jsx';
 import GlobalHomeroomNavigationTab from './GlobalHomeroomNavigationTab.jsx';
 import GlobalGradebookNavigationTab from './GlobalGradebookNavigationTab.jsx';
 import GlobalReportsNavigationTab from './GlobalReportsNavigationTab.jsx';
-import GlobalTtcmNavigationTab from './GlobalTtcmNavigationTab.jsx';
+import GlobalTtcmMobileAdapter from './GlobalTtcmMobileAdapter.jsx';
 import GlobalAttendanceNavigationTab from './GlobalAttendanceNavigationTab.jsx';
 import GlobalAttendanceAdminPersistenceBridge from './GlobalAttendanceAdminPersistenceBridge.jsx';
 import GlobalDashboardFooterBridge from './GlobalDashboardFooterBridge.jsx';
@@ -104,7 +104,7 @@ export default function GlobalFlatNavigation(props) {
       <GlobalHomeroomNavigationTab {...props} />
       <GlobalGradebookNavigationTab {...props} />
       <GlobalReportsNavigationTab {...props} />
-      <GlobalTtcmNavigationTab {...props} />
+      <GlobalTtcmMobileAdapter {...props} />
       <GlobalAttendanceNavigationTab {...props} />
       <GlobalAttendanceAdminPersistenceBridge {...props} />
       {!mobile ? <GlobalDashboardFooterBridge route={props.route} language={props.language} /> : null}
