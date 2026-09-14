044f43d50661fa0fa69631479631e657916a68f6
anhtuan-hash <anhtuan@pek.edu.vn>
2026-09-14 10:22:11 +0700

    fix(mobile): assemble embedded Brian hero image

---
+++ src/assets/mobileHero/index.js
@@ -0,0 +1,6 @@
+import p1 from './part01.js';
+import p2 from './part02.js';
+import p3 from './part03.js';
+import p4 from './part04.js';
+
+export default 'data:image/webp;base64,' + p1 + p2 + p3 + p4;
