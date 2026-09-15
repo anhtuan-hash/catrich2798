8564a6d6b8a88a36e548b0a13283618f101a8db8
anhtuan-hash <anhtuan@pek.edu.vn>
2026-09-15 22:19:24 +0700

    style: activate pastel navigation palette

--- src/components/GlobalFlatNavigation.jsx
+++ src/components/GlobalFlatNavigation.jsx
@@ -50,6 +50,7 @@ import './GlobalNotificationCenterFinalRemoval.css';
 import './GlobalHomeSparkleButton.css';
 import './homeroom/HomeroomCompactDensity.css';
 import '../styles/GlobalLayout16x9Authority.css';
+import './GlobalNavigationPastelPalette.css';
 
 export default function GlobalFlatNavigation(props) {
   const presentation = usePresentationMode();
